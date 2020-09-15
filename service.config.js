9a37376987f7e4b7bf6854aaa5d917b76dbe0150
qiangfeng <qiangfeng@winning.com.cn>
2020-09-15 10:18:58 +0800

    chang cli scripts

--- service.config.js
+++ service.config.js
@@ -1,24 +1,25 @@
 module.exports = {
-  build: {
-    dist: 'dist', // 生产包存放目录
-    packages: 'packages' // 子应用默认路径
+  'data': {
+    'applicationList': [
+      {
+        'appSystemId': '4302708202',
+        'appSystemName': '门诊费用管理',
+        'appMenuList': [
+          {
+            'name': 'project1',
+            'appMenuId': '4302708264',
+            'appMenuRelativeUri': '/fee-management-outp/project1',
+            'appMenuName': '测试页面',
+            'appMenuList': [],
+            'devEntry': 'http://localhost:3003'
+          }
+        ]
+      }
+    ],
+    'currentAppId': '4302708202'
   },
-  data: {
-    applicationList: [{
-      appSystemId: '4302708202',
-      appSystemName: '门诊费用管理',
-      appSystemPicUrl: 'c:\\',
-      appSystemRelativeUri: 'fee-management-outp',
-      appMenuList: [
-        {
-          appMenuId: '4302708264',
-          appMenuRelativeUri: '/#/feeCompare',
-          appMenuName: '费用对比',
-          appMenuList: [],
-          devEntry: 'http://localhost:3003'
-        }
-      ]
-    }],
-    currentAppId: '4302708202'
+  'build': {
+    'dist': 'dist',
+    'packages': 'packages'
   }
 }
