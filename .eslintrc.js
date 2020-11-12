51bef71e82067e68c6e83cd98fe6f8c4fd74cdf8
fengqiang <1649271411@qq.com>
2020-11-12 17:14:38 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,8 @@ module.exports = {
   },
   extends: [
     'plugin:vue/essential',
-    '@vue/standard'
+    '@vue/airbnb',
+    'eslint:recommended'
   ],
   parserOptions: {
     parser: 'babel-eslint'
