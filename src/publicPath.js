bdd9efb8b2b8d7b0df495ae8196ef4904497e57e
small.also <hu141418@gmail.com>
2020-12-08 18:14:54 +0800

    new template

--- src/publicPath.js
+++ src/publicPath.js
@@ -1,5 +1,13 @@
 /* eslint-disable */
+
 const isProduction = process.env.NODE_ENV === 'production'
+const isLocal = process.env.HIS_MICRO_APP_DEVELOP_MODE === 'local'
+
+// 解决window下 多
+if (!isProduction && isLocal) {
+  const  Vue = require('vue')
+  Vue && Vue.default && (window.Vue = Vue.default)
+}
 if (window.__POWERED_BY_QIANKUN__) {
-    __webpack_public_path__ = isProduction ? window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__ : process.env.VUE_APP_SYSTEM_BASE_URL;
-  }
+  __webpack_public_path__ = isProduction ? window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__ : process.env.VUE_APP_SYSTEM_BASE_URL;
+}
