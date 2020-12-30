6492dab9a1165f9616e0ecf562114aa02743abfa
smallalso <31307699+smallalso@users.noreply.github.com>
2020-12-30 17:53:10 +0800

    Update publicPath.js

--- src/publicPath.js
+++ src/publicPath.js
@@ -8,6 +8,6 @@ if (!isProduction && isLocal) {
   const  Vue = require('vue')
   Vue && Vue.default && (window.Vue = Vue.default)
 }
-if (window.__POWERED_BY_QIANKUN__) {
-  __webpack_public_path__ = isProduction ? window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__ : process.env.VUE_APP_SYSTEM_BASE_URL;
-}
+// if (window.__POWERED_BY_QIANKUN__) {
+//  __webpack_public_path__ = isProduction ? window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__ : process.env.VUE_APP_SYSTEM_BASE_URL;
+// }
